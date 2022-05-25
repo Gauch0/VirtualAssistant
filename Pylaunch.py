a605670376b0e6a71229f3273215a74ee451d732
Gauch0 <matiaslorenzo466@gmail.com>
2022-05-25 20:25:25 -0300

    fix

--- Pylaunch.py
+++ Pylaunch.py
@@ -1,9 +1,33 @@
 import pyttsx3
 import speech_recognition as rs 
 
-listener = rs.Recognizer()
+nombre = 'Matthias'
 
-try:
-    with rs.Microphone() as 
-except:
-    pass
+listener = rs.Recognizer()
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voices', voices[1].id)
+
+
+def talk(texto):
+    engine.say(texto)
+    engine.runAndWait()
+
+def lisen():
+    try:
+        with rs.Microphone() as source:
+            print("Esto se esta escuchando..!")
+            voice = listener.listen(source)
+            rec = listener.recognize_google(voice)
+            if nombre in rec:
+                print(talk(rec))
+    except:
+        pass
+    return rec
+
+def run():
+    rec = lisen()
+    if 'reproduce' in rec:
+        print('Reproduciendo')
+run()
